cb463ac45e56a5eb60aba30b6165bda86fe7ee0d
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-05 06:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// Optional: No JS is required for this task, but file is included for completeness
-console.log("Nested HTML Tables Loaded");
+// No logic needed for the task
+console.log("Script Loaded");
